a07210ff5084009d55aa7c7df168c1e3695323c4
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-04-11 14:17:46 -0300

    feat: add the Home page using the Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import HomeComponent from '@/pagesComponents/Home';
+import { Box } from '@chakra-ui/react';
+
 export default function Home() {
-  return (
-    <div>Fodase</div>
-  );
+	return (
+		<Box className='flex min-h-screen flex-col items-center justify-center p-24'>
+			<HomeComponent />
+		</Box>
+	);
 }
