b4b564bbff003821666e08adc7e67fb475a794dc
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-04-11 14:16:11 -0300

    feat: add the 'createArray' function, that returns an array with the roles that has 'true'

---
+++ src/utils/createArray.ts
@@ -0,0 +1,9 @@
+import { ChampionDamageType, ChampionReturn, Roles } from '@/types/championReturn';
+
+export const createArray = (preset: Roles[] | ChampionDamageType[], champion: ChampionReturn) => {
+  const array = [];
+  for (let i = 0; i < preset.length; i++) {
+    if(champion[preset[i]]) array.push(preset[i]);
+  }
+  return array;
+}
