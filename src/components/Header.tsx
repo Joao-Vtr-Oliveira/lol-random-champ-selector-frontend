8eb2d69ad7c1c36737bc9d664e005e0853d3fed2
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-04-13 15:19:43 -0300

    chores: change the header icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,13 @@
 'use client';
 
 import { Box, Icon, Text } from '@chakra-ui/react';
-import { IoHome } from "react-icons/io5";
 import { IoDice } from "react-icons/io5";
 import { FaPlus } from "react-icons/fa";
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { verifyRoute } from '@/utils/verifyRoute';
 import { hoverBtn } from '@/utils/hoverBtn';
+import { FaList } from "react-icons/fa6";
 
 
 const Header = () => {
@@ -16,14 +16,14 @@ const Header = () => {
   return(
     <Box w={'100%'} className='h-1/6 bg-gradient-to-r from-indigo-800 via-purple-800 to-pink-800' display='flex' justifyContent='space-around' alignItems='center' textColor='black'>
       <Link href={'/'}>
-        <Icon boxSize={8} color={verifyRoute(pathname, '/') ? '#FA2083' : 'white'} _hover={verifyRoute(pathname, '/') ? {} : hoverBtn} as={IoHome} />
+        <Icon boxSize={8} color={verifyRoute(pathname, '/') ? '#FA2083' : 'white'} _hover={verifyRoute(pathname, '/') ? {} : hoverBtn} as={IoDice} />
       </Link>
       <Link href={'/'}>
         <Text color={verifyRoute(pathname, '/') ? '#FA2083' : 'white'} textDecoration={verifyRoute(pathname, '/') ? 'underline' : 'none'} fontFamily='fantasy'>LoL Random Champ Selector</Text>
       </Link>
       <Box>
-        <Link href={'/random'}>
-          <Icon color={verifyRoute(pathname, '/random') ? '#FA2083' : 'white'} _hover={verifyRoute(pathname, '/random') ? {} : hoverBtn} boxSize={8} mr={3} as={IoDice} />
+        <Link href={'/list'}>
+          <Icon color={verifyRoute(pathname, '/list') ? '#FA2083' : 'white'} _hover={verifyRoute(pathname, '/list') ? {} : hoverBtn} boxSize={8} mr={3} as={FaList} />
         </Link>
         <Link href={'/new'}>
           <Icon color={verifyRoute(pathname, '/new') ? '#FA2083' : 'white'} _hover={verifyRoute(pathname, '/new') ? {} : hoverBtn} boxSize={8} mr={3} as={FaPlus} />
