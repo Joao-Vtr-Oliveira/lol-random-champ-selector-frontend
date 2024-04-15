dfc9289493f1b6ebf60ff9264dc504e6e6f6a88a
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-04-15 10:57:05 -0300

    feat: add some custom colors to the chakra provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,14 @@
 'use client';
 
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+
+const colors = {
+	customPurple: '#570FA0',
+	customPinky: '#FA2083',
+}
+
+const theme = extendTheme({ colors });
 
 export function Providers({ children }: { children: React.ReactNode }) {
-	return <ChakraProvider>{children}</ChakraProvider>;
+	return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
 }
