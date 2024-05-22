31affe569a63f77c95b68ddddf8f41b188edf860
João V <joao.vtr.oliveira@outlook.com>
2024-05-22 12:13:12 -0300

    chores: add a log for debugging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import Random from '@/pagesComponents/Random';
+import url from '@/utils/pickEnvVar';
 import { Box } from '@chakra-ui/react';
 import { Metadata } from 'next';
+
 
 export const metadata: Metadata = {
 	title: 'Random Champion',
 };
 
+
 const RandomPage = () => {
+  console.log(url);
+
   return(
     <Box className='flex h-5/6 flex-col items-center '>
       <Random />
