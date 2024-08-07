e5c727583e066aa7c9ec907d10e528bd948c103d
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-08-07 17:33:31 -0300

    feat: add an image for each champion

--- src/components/ChampionCard.tsx
+++ src/components/ChampionCard.tsx
@@ -1,39 +1,95 @@
 import { ChampionReturn } from '@/types/championReturn';
 import { damageTypesArray, rolesArray } from '@/utils/championInfo';
 import { createArray } from '@/utils/createArray';
-import { Box, Button, Card, CardBody, CardFooter, CardHeader, Heading } from '@chakra-ui/react';
+import { specifChampionName } from '@/utils/specificChampioName';
+import {
+	Box,
+	Button,
+	Card,
+	CardBody,
+	CardFooter,
+	CardHeader,
+	Heading,
+	Img,
+} from '@chakra-ui/react';
 import Link from 'next/link';
 
-const ChampionCard = ({ champion }: { champion: ChampionReturn }) => {  
-  const roles = createArray(rolesArray, champion);
-  const types = createArray(damageTypesArray, champion);
+const ChampionCard = ({ champion }: { champion: ChampionReturn }) => {
+	const roles = createArray(rolesArray, champion);
+	const types = createArray(damageTypesArray, champion);
+
+	const name = specifChampionName(champion.name);
+	const link = `https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${name}_0.jpg`;
+
 
 	return (
-		<Card display={'flex'} mb={10} flexDirection={'column'}>
+		<Card
+			display={'flex'}
+			mb={10}
+			flexDirection={'column'}
+		>
 			<CardHeader textAlign='center'>
-        {champion.nameBase !== '-' &&
-        <Link target='_blank' href={`https://www.leagueofgraphs.com/champions/builds/${champion.nameBase}`}>
-				  <Heading fontSize='2xl' color='customPurple' _hover={{'textDecoration': 'underline'}}>{champion.name}</Heading>
-        </Link>
-        }
-        {champion.nameBase === '-' &&
-				  <Heading fontSize='2xl' color='customPurple'>{champion.name}</Heading>
-        }
+				{champion.nameBase !== '-' && (
+					<Link
+						target='_blank'
+						href={`https://www.leagueofgraphs.com/champions/builds/${champion.nameBase}`}
+					>
+						<Heading
+							fontSize='2xl'
+							color='customPurple'
+							_hover={{ textDecoration: 'underline' }}
+						>
+							{champion.name}
+						</Heading>
+					</Link>
+				)}
+				{champion.nameBase === '-' && (
+					<Heading
+						fontSize='2xl'
+						color='customPurple'
+					>
+						{champion.name}
+					</Heading>
+				)}
 			</CardHeader>
 			<CardBody>
-				<Box fontWeight='bold' textAlign='center'>Roles: {roles.join(', ')}</Box>
-				<Box fontWeight='bold' textAlign='center'>Types: {types.join(', ')}</Box>
-				<Box fontWeight='bold' textAlign='center'>Range: {champion.ranged === true ? 'Ranged' : 'Melee'}</Box>
+				<Link
+					target='_blank'
+					href={`https://www.leagueofgraphs.com/champions/builds/${champion.nameBase}`}
+				>
+					<Img
+						src={link}
+						borderRadius={10}
+					/>
+				</Link>
+				<Box
+					fontWeight='bold'
+					textAlign='center'
+				>
+					Roles: {roles.join(', ')}
+				</Box>
+				<Box
+					fontWeight='bold'
+					textAlign='center'
+				>
+					Types: {types.join(', ')}
+				</Box>
+				<Box
+					fontWeight='bold'
+					textAlign='center'
+				>
+					Range: {champion.ranged === true ? 'Ranged' : 'Melee'}
+				</Box>
 			</CardBody>
-      <CardFooter>
-        <Box margin='auto'>
-          {champion.nameBase !== '-' &&
-          <Link href={`/champion/${champion.nameBase}`}>
-            <Button colorScheme='yellow'>Edit</Button>
-          </Link>
-          }
-        </Box>
-      </CardFooter>
+			<CardFooter>
+				<Box margin='auto'>
+					{champion.nameBase !== '-' && (
+						<Link href={`/champion/${champion.nameBase}`}>
+							<Button colorScheme='yellow'>Edit</Button>
+						</Link>
+					)}
+				</Box>
+			</CardFooter>
 		</Card>
 	);
 };
